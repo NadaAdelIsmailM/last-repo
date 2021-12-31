328a3f471e0cc8788cccf7ad8daf90f6ea55f38a
Farreeda <94711213+Farreeda@users.noreply.github.com>
2022-01-01 00:12:51 +0200

    Add files via upload

---
+++ AddModule.cpp
@@ -0,0 +1,20 @@
+#include "AddModule.h"
+AddModule::AddModule(ApplicationManager* pApp) :Action(pApp)
+{
+}
+
+AddModule::~AddModule(void)
+{
+}
+
+void AddModule::Execute()
+{
+
+
+}
+
+void AddModule::Undo()
+{}
+
+void AddModule::Redo()
+{}
